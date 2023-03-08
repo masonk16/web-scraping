16f2529fd4e9b4a0b8f45c1ae4f5c673e6e9a1d2
Mason Mapfundematsva <masonmapfunde@gmail.com>
2023-03-08 20:47:26 +0200

    Change url

--- main.py
+++ main.py
@@ -2,10 +2,11 @@
 from bs4 import BeautifulSoup
 import requests
 
-url = "https://steamdb.info/charts/"
+url = "https://www.audible.com/search?keywords=book&node=18573211011&pageSize=50&ref=a_search_c4_pageSize_3&pf_rd_p=1d79b443-2f1d-43a3-b1dc-31a2cd242566&pf_rd_r=68FN4HT4VR3SX6M9MTA0&pageLoadId=hkWBGfVm3NAVPAx6&creativeId=18cc2d83-2aa9-46ca-8a02-1d1cc7052e2a"
 
 response = requests.get(url)
+print(response.text)
 
-soup = BeautifulSoup(response.text, "html")
+soup = BeautifulSoup(response.text, "html.parser")
 
 
