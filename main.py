0c485706529df4ba699944522fe998be3c3bf12f
Mason Mapfundematsva <masonmapfunde@gmail.com>
2023-03-08 22:41:21 +0200

    Remove authors and narrators.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env/ python3
 import csv
+import pprint
 from itertools import zip_longest
 from bs4 import BeautifulSoup
 import requests
@@ -13,8 +14,6 @@ soup = BeautifulSoup(response.text, "html.parser")
 
 # Scrape Data
 book_names = soup.select("h3 a")
-authors = soup.select("a[href^='/author/']")
-narrators = soup.select("a[href^='/search?searchNarrator=']")
 runtimes = soup.select("[class~=runtimeLabel] > span")
 release_dates = soup.select("[class~=releaseDateLabel] > span")
 languages = soup.select("[class~=languageLabel] > span")
@@ -22,11 +21,14 @@ prices = soup.select("[class~=buybox-regular-price] > span:nth-of-type(2)")
 
 # Extract text  and clean data
 clean_book_names = [book_name.get_text() for book_name in book_names]
-clean_authors = [author.get_text() for author in authors]
-clean_narrators = [narrator.get_text() for narrator in narrators]
 clean_runtimes = [runtime.get_text().split(":")[1].strip() for runtime in runtimes]
 clean_release_dates = [release_date.get_text().split(":")[1].strip() for release_date in release_dates]
 clean_languages = [language.get_text().split(":")[1].strip() for language in languages]
 clean_prices = [price.get_text(strip=True) for price in prices]
 
-
+# # Create csv file
+# with open('computers-and-technology-audiobooks.csv', 'w') as file:
+#     writer = csv.writer(file)
+#     writer.writerow(["Book Name", "Length" "Release Date", "Language", "Price"])
+#     for row in zip_longest(clean_book_names, clean_runtimes, clean_release_dates, clean_languages, clean_prices):
+#         writer.writerow(row)
